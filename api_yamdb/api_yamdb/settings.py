9d95b63febc8cd8d916dd9b676a5907c0ce4ba9f
Василий <vasyakuzmin1@yandex.ru>
2022-07-31 14:02:26 +0300

    domain delete

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,14 +11,10 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-HOST = os.getenv('HOST')
-DOMAIN = os.getenv('DOMAIN')
-
 ALLOWED_HOSTS = [
-    HOST,
+    '*',
     'localhost',
     '127.0.0.1',
-    DOMAIN,
 ]
 
 
